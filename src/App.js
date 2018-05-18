18692f5ae149bf0358837e31b253ccaa7fc89310
Anna <annaoh1994@gmail.com>
2018-05-17 23:08:22 -0700

    Testing API get requests

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { getCities } from './util/location_util';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      input: '',
+      cities: []
+    };
+  }
+
+  handleSubmit() {
+    if (this.state.input === '') return null;
+
+    getCities(this.state.input).then(res => {
+      let cities = [];
+      res.forEach(city => {
+        cities.push(city.title);
+      });
+      this.setState({
+        [cities]: cities
+      });
+    });
+  }
+
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>AO</h1>
+        <h1>ANNA OH</h1>
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.update('input')}
+        />
+        <input type="submit" onClick={this.handleSubmit} />
       </div>
     );
   }
