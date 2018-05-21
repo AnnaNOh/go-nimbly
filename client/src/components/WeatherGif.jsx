cdcbcfab001c8513bcaf796ab67b00078af38d7a
Anna <annaoh1994@gmail.com>
2018-05-21 13:52:09 -0700

    Add gifs to assets and Weathertoday displays all the info

---
+++ client/src/components/WeatherGif.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import '../assets/stylesheets/weatherToday.css';
+
+import sn from '../assets/gifs/sn-1.gif';
+import sl from '../assets/gifs/sl-1.gif';
+import h from '../assets/gifs/h-1.gif';
+import t from '../assets/gifs/t-1.gif';
+import hr from '../assets/gifs/hr-1.gif';
+import lr from '../assets/gifs/lr-1.gif';
+import s from '../assets/gifs/s-1.gif';
+import hc from '../assets/gifs/hc-1.gif';
+import lc from '../assets/gifs/lc-1.gif';
+import c from '../assets/gifs/c-1.gif';
+
+class WeatherGif extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let weather = this.props.weather;
+    if (weather === 'lc') {
+      return <img className="weather-gif" alt="lc" src={lc} />;
+    } else if (weather === 'sn') {
+      return <img className="weather-gif" alt="sn" src={sn} />;
+    } else if (weather === 'sl') {
+      return <img className="weather-gif" alt="sl" src={sl} />;
+    } else if (weather === 'h') {
+      return <img className="weather-gif" alt="h" src={h} />;
+    } else if (weather === 't') {
+      return <img className="weather-gif" alt="t" src={t} />;
+    } else if (weather === 'hr') {
+      return <img className="weather-gif" alt="hr" src={hr} />;
+    } else if (weather === 'lr') {
+      return <img className="weather-gif" alt="lr" src={lr} />;
+    } else if (weather === 's') {
+      return <img className="weather-gif" alt="s" src={s} />;
+    } else if (weather === 'hc') {
+      return <img className="weather-gif" alt="hc" src={hc} />;
+    } else if (weather === 'c') {
+      return <img className="weather-gif" alt="c" src={c} />;
+    }
+  }
+}
+
+export default WeatherGif;
--- client/src/components/WeatherToday.jsx
+++ client/src/components/WeatherToday.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../assets/stylesheets/weatherToday.css';
+import WeatherGif from './WeatherGif';
+
+import lc1 from '../assets/gifs/lc-1.gif';
 
 class WeatherToday extends Component {
   constructor(props) {
@@ -8,14 +11,39 @@ class WeatherToday extends Component {
   }
 
   render() {
-    // console.log(this.state);
     console.log(this.props);
 
-    return (
-      <div className="">
-        <h1>hi I'm weather today</h1>
-      </div>
-    );
+    if (this.props.city.length <= 0) {
+      return (
+        <div>
+          <h1>Looking Outside For You</h1>
+        </div>
+      );
+    } else {
+      let weather = this.props.weather[0];
+      return (
+        <div className="weather-today">
+          <h2 className="weather-city">{this.props.city}</h2>
+          <WeatherGif weather={weather.weather_state_abbr} />
+          <div className="weather-report">
+            <div className="weather-report-left">
+              <h3 className="weather-report-date">RIGHT NOW</h3>
+              <h3 className="weather-report-status">
+                {String(weather.weather_state_name).toUpperCase()}
+              </h3>
+            </div>
+            <div className="weather-report-right">
+              <h3 className="weather-report-degree">
+                {Math.round(weather.max_temp)}
+              </h3>
+              <h3 className="weather-report-degree">
+                {Math.round(weather.min_temp)}
+              </h3>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
